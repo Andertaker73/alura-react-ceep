c25b7cf9e53b96ac7969877ea969b27ca761c0ed
Anderson Veeck <andersonveeck@gmail.com>
2022-08-29 23:24:37 -0300

    lesson 2.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react"
+import ListaDeNotas from "./components/ListaDeNotas"
+
 function App() {
   return (
-    <h1>My react app</h1>
-  );
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas />
+    </section>
+  )
 }
 
-export default App;
+export default App
