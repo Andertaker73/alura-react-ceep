3323131c53d1b68b2bcad4ace6e5d1a89d76f713
Anderson Veeck <andersonveeck@gmail.com>
2022-09-19 21:14:11 -0300

    lesson 5.4

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -6,10 +6,10 @@ class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+        {this.props.notas.map((notas, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={notas.titulo} texto={notas.texto} />
             </li>
           )
         })}
